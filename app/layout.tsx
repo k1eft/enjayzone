5bafb3acc387ec0f5a006f79382652661e0e126e
k1eft <pabosikief@gmail.com>
2025-12-23 19:33:19 +0800

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -65,6 +66,7 @@ export default function RootLayout({
           </main>
 
         </div>
+      <Analytics />
       </body>
     </html>
   );
